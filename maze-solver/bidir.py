fd7055184863f7f4d86c45575e8ffff6829a46b9
Isha Gupta <156700367+isha06gupta@users.noreply.github.com>
2026-02-18 09:46:52 +0530

    Update bidir.py

--- maze-solver/bidir.py
+++ maze-solver/bidir.py
@@ -1,4 +1,50 @@
+from collections import deque
+import networkx as nx
+import matplotlib.pyplot as plt
+
+# -------------------------------
+# City Map (Graph Representation)
+# -------------------------------
+
+graph = {
+    'A': ['B', 'D'],
+    'B': ['A', 'C', 'E'],
+    'C': ['B', 'F'],
+    'D': ['A', 'E', 'G'],
+    'E': ['B', 'D', 'F'],
+    'F': ['C', 'E', 'I'],
+    'G': ['D', 'H'],
+    'H': ['G', 'I'],
+    'I': ['F', 'H']
+}
+
+# --------------------------------
+# Helper Function to Build Path
+# --------------------------------
+
+def build_path(parent_start, parent_goal, meeting):
+    path_start = []
+    node = meeting
+    while node is not None:
+        path_start.append(node)
+        node = parent_start[node]
+    path_start.reverse()
+
+    path_goal = []
+    node = parent_goal[meeting]
+    while node is not None:
+        path_goal.append(node)
+        node = parent_goal[node]
+
+    return path_start + path_goal
+
+
+# --------------------------------
+# Bi-Directional BFS Function
+# --------------------------------
+
 def bidirectional_bfs(graph, start, goal):
+
     if start == goal:
         return [start], 0
 
@@ -12,36 +58,76 @@ def bidirectional_bfs(graph, start, goal):
     parent_goal = {goal: None}
 
     explored = 0
-    meeting_node = None
 
     while queue_start and queue_goal:
 
-        # Expand from start side
-        node = queue_start.popleft()
+        # ---- Expand from start side ----
+        current_start = queue_start.popleft()
         explored += 1
 
-        for neighbor in graph[node]:
+        for neighbor in graph[current_start]:
             if neighbor not in visited_start:
                 visited_start.add(neighbor)
-                parent_start[neighbor] = node
+                parent_start[neighbor] = current_start
                 queue_start.append(neighbor)
 
                 if neighbor in visited_goal:
-                    meeting_node = neighbor
-                    return build_bidirectional_path(parent_start, parent_goal, meeting_node), explored
+                    path = build_path(parent_start, parent_goal, neighbor)
+                    return path, explored
 
-        # Expand from goal side
-        node = queue_goal.popleft()
+        # ---- Expand from goal side ----
+        current_goal = queue_goal.popleft()
         explored += 1
 
-        for neighbor in graph[node]:
+        for neighbor in graph[current_goal]:
             if neighbor not in visited_goal:
                 visited_goal.add(neighbor)
-                parent_goal[neighbor] = node
+                parent_goal[neighbor] = current_goal
                 queue_goal.append(neighbor)
 
                 if neighbor in visited_start:
-                    meeting_node = neighbor
-                    return build_bidirectional_path(parent_start, parent_goal, meeting_node), explored
+                    path = build_path(parent_start, parent_goal, neighbor)
+                    return path, explored
 
     return None, explored
+
+
+# --------------------------------
+# Run the Search
+# --------------------------------
+
+start = 'A'
+goal = 'I'
+
+path, explored_nodes = bidirectional_bfs(graph, start, goal)
+
+print("Shortest Path using Bi-Directional BFS:")
+print(path)
+print("Number of Nodes Explored:", explored_nodes)
+
+
+# --------------------------------
+# Visualization using networkx
+# --------------------------------
+
+G = nx.Graph()
+
+for node in graph:
+    for neighbor in graph[node]:
+        G.add_edge(node, neighbor)
+
+pos = nx.spring_layout(G)
+
+plt.figure(figsize=(8, 6))
+
+# Draw all nodes
+nx.draw(G, pos, with_labels=True, node_color='lightblue', node_size=2000)
+
+# Highlight path if found
+if path:
+    nx.draw_networkx_nodes(G, pos, nodelist=path, node_color='red')
+    edges_in_path = list(zip(path, path[1:]))
+    nx.draw_networkx_edges(G, pos, edgelist=edges_in_path, edge_color='red', width=3)
+
+plt.title("City Route Finder using Bi-Directional BFS")
+plt.show()
